d4fe56a3281e978ca893477d2ec678a569d3a2e4
CYMN16 <c.yaman2002@gmail.com>
2024-02-17 13:42:15 +0300

    fixed rendering and displaying of rendered image

--- src/display.rs
+++ src/display.rs
@@ -18,9 +18,77 @@ struct AppState {
 }
 
 impl AppState {
-    pub fn update_image(&mut self, img: Image) {
+    pub fn update_image(&mut self) {
+        let img:Image = render(&mut self.cam, &self.world).into();
         self.image_buf = img.into();
     }
+}
+
+/// builds a child Flex widget from some parameters.
+struct Rebuilder {
+    inner: Box<dyn Widget<AppState>>,
+}
+
+impl Rebuilder {
+    fn new() -> Rebuilder {
+        Rebuilder {
+            inner: SizedBox::empty().boxed(),
+        }
+    }
+
+    fn rebuild_inner(&mut self, data: &AppState) {
+        self.inner = build_widget(data);
+    }
+}
+
+impl Widget<AppState> for Rebuilder {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppState, env: &Env) {
+        // data.update_image();
+        self.inner.event(ctx, event, data, env) 
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &AppState, env: &Env) {
+        if let LifeCycle::WidgetAdded = event {
+            self.rebuild_inner(data);
+        }
+
+        self.inner.lifecycle(ctx, event, data, env)
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppState, data: &AppState, _env: &Env) {
+        if !old_data.same(data) {
+            self.rebuild_inner(data);
+            ctx.children_changed();
+        }
+    }
+
+    fn layout(
+        &mut self,
+        ctx: &mut LayoutCtx,
+        bc: &BoxConstraints,
+        data: &AppState,
+        env: &Env,
+    ) -> Size {
+        self.inner.layout(ctx, bc, data, env)
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, env: &Env) {
+        self.inner.paint(ctx, data, env)
+    }
+
+    fn id(&self) -> Option<WidgetId> {
+        self.inner.id()
+    }
+}
+
+fn build_widget(state: &AppState) -> Box<dyn Widget<AppState>> {
+    let png_data = state.image_buf.clone();
+
+    let img = druid::widget::Image::new(png_data);
+
+    let sized = SizedBox::new(img);
+
+    sized.border(Color::grey(0.6), 2.0).center().boxed()
 }
 
 fn render(cam: &mut Camera, world: &HittableList) -> Image {
@@ -37,21 +105,14 @@ fn build_ui(app_state: &AppState, cam: &mut Camera, world: &HittableList) -> imp
     Flex::column()
         .with_child(
             Flex::row()
-                .with_child(
-                    // Viewport
-                    Container::new(druid::widget::Image::new(app_state.image_buf.clone()).center())
-                    // druid::widget::Image::new(app_state.image_buf.clone())
-                    .fix_size(800., 800.*9./16.)
-                    .padding(10.0)
-                    .border(Color::BLACK, 1.0)
-                    .center()
-                    .lens(AppState::image_buf)
+                .with_flex_child(Rebuilder::new().center(), 1.0
                 )
                 .with_child(
                     // Render Button
                     Button::new("Render").on_click(move |ctx, app_state: &mut AppState, _| {
-                        let img = render(&mut app_state.cam, &app_state.world).into();
-                        app_state.update_image(img);
+                        // let img = render(&mut app_state.cam, &app_state.world).into();
+                        app_state.update_image();
+                        // ctx.request_update();
                     }),
                 )
                 .cross_axis_alignment(CrossAxisAlignment::Center)
